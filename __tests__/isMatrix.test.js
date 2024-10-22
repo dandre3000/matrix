1a46fe308ee502a5a542899a2fa3318e9be3cd03
dandre3000 <deaundre.payne@gmail.com>
2024-10-21 23:44:50 -0400

    Update isMatrix.test.js

--- __tests__/isMatrix.test.js
+++ __tests__/isMatrix.test.js
@@ -6,224 +6,193 @@ test('return false if matrix is not defined', () => {
 	expect(isMatrix(undefined)).toBe(false)
 })
 
-test('return false if property rows, columns or length of argument matrix are not defined', () => {
+test('return false if matrix.rows or matrix.columns are not defined', () => {
 	expect(isMatrix([0])).toBe(false)
 	expect(isMatrix({
-		0: 0,
-		columns: 1,
-		length: 1
+		data: [0],
+		columns: 1
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
+		data: [0],
 		rows: undefined,
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
+		data: [0],
+		rows: 1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
 		rows: 1,
-		length: 1
+		columns: undefined
+	})).toBe(false)
+})
+
+test('return false if matrix.rows or matrix.columns are not numbers', () => {
+	expect(isMatrix({
+		data: [0],
+		rows: null,
+		columns: 1
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
+		data: [0],
+		rows: true,
+		columns: 1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: '1',
+		columns: 1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: Symbol(),
+		columns: 1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: {},
+		columns: 1
+	})).toBe(false)
+	
+	expect(isMatrix({
+		data: [0],
 		rows: 1,
-		columns: undefined,
-		length: 1
+		columns: null
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
+		data: [0],
+		rows: 1,
+		columns: true
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: 1,
+		columns: '1'
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: 1,
+		columns: Symbol()
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: 1,
+		columns: {}
+	})).toBe(false)
+})
+
+test('return false if matrix.rows or matrix.columns are not integers', () => {
+	expect(isMatrix({
+		data: [0],
+		rows: 1.1,
+		columns: 1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: 1.9,
+		columns: 1
+	})).toBe(false)
+	
+	expect(isMatrix({
+		data: [0],
+		rows: 1,
+		columns: 1.1
+	})).toBe(false)
+	expect(isMatrix({
+		data: [0],
+		rows: 1,
+		columns: 1.9
+	})).toBe(false)
+})
+
+test('return false if matrix.data.length is not defined', () => {
+	expect(isMatrix({
+		data: {
+			0: 0
+		},
+		rows: 1,
+		columns: 1
+	})).toBe(false)
+	
+	expect(isMatrix({
+		data: {
+			0: 0,
+			length: undefined
+		},
+		rows: 1,
+		columns: 1
+	})).toBe(false)
+})
+
+test('return false if matrix.data.length does not equal matrix.rows * matrix.columns', () => {
+	expect(isMatrix({
+		data: {
+			0: 0,
+			1: 0,
+			2: 0,
+			3: 0,
+			length: 3
+		},
+		rows: 2,
+		columns: 2
+	})).toBe(false)
+	
+	expect(isMatrix({
+		data: {
+			0: 0,
+			1: 0,
+			2: 0,
+			3: 0,
+			length: 5
+		},
+		rows: 2,
+		columns: 2
+	})).toBe(false)
+})
+
+test('return false if any number indexed property of matrix.data is not a number', () => {
+	expect(isMatrix({
+		data: [undefined],
 		rows: 1,
 		columns: 1
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: undefined
-	})).toBe(false)
-})
-
-test('return false if property rows, columns or length of argument matrix are not numbers', () => {
-	expect(isMatrix({
-		0: 0,
-		rows: null,
-		columns: 1,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: true,
-		columns: 1,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: '1',
-		columns: 1,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: Symbol(),
-		columns: 1,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: {},
-		columns: 1,
-		length: 1
-	})).toBe(false)
-	
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: null,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: true,
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: '1',
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: Symbol(),
-		length: 1
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: {},
-		length: 1
-	})).toBe(false)
-	
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: null
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: true
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: '1'
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: Symbol()
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: {}
-	})).toBe(false)
-})
-
-test('return false if property rows or columns of argument matrix is not an integer', () => {
-	expect(isMatrix({
-		0: 0,
-		rows: 1.1,
-		columns: 1,
-		length: {}
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1.1,
-		length: {}
-	})).toBe(false)
-})
-
-test('return false if property length of argument matrix does not equal property rows times property columns', () => {
-	expect(isMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
+		data: [0, null],
 		rows: 2,
-		columns: 2,
-		length: 3
+		columns: 1
 	})).toBe(false)
-})
-
-test('return false if any enumerable property of argument matrix is not a number', () => {
 	expect(isMatrix({
-		0: undefined,
+		data: [0, 0, true],
 		rows: 1,
-		columns: 1,
-		length: 1
+		columns: 3
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
-		0: null,
+		data: [0, 0, 0, '0'],
 		rows: 2,
-		columns: 1,
-		length: 2
+		columns: 2
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
-		0: 0,
-		0: true,
-		rows: 1,
-		columns: 3,
-		length: 3
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: '0',
-		rows: 2,
-		columns: 2,
-		length: 4
-	})).toBe(false)
-	expect(isMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: Symbol(),
+		data: [0, 0, 0, 0, Symbol()],
 		rows: 5,
-		columns: 1,
-		length: 5
+		columns: 1
 	})).toBe(false)
 	expect(isMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: {},
+		data: [0, 0, 0, 0, 0, {}],
 		rows: 2,
-		columns: 3,
-		length: 6
+		columns: 3
+	})).toBe(false)
+	expect(isMatrix({
+		data: new Array(6),
+		rows: 2,
+		columns: 3
 	})).toBe(false)
 })
 
 test('return true if argument matrix fits the Matrix interface', () => {
 	expect(isMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toBe(true)
 })
