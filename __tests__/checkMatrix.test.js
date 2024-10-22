0284153b885fd34eee3dd013617100168493ff94
dandre3000 <deaundre.payne@gmail.com>
2024-10-21 23:44:58 -0400

    Update checkMatrix.test.js

--- __tests__/checkMatrix.test.js
+++ __tests__/checkMatrix.test.js
@@ -1,220 +1,200 @@
 import { expect, test } from '@jest/globals'
 import { checkMatrix } from '../main.js'
 
-test('throw a ReferenceError if matrix is not defined', () => {
+test('Throw ReferenceError if matrix is not defined', () => {
 	expect(() => checkMatrix()).toThrow(ReferenceError)
 	expect(() => checkMatrix(undefined)).toThrow(ReferenceError)
 })
 
-test('throw a ReferenceError if property rows, columns or length of argument matrix are not defined', () => {
+test('Throw ReferenceError if matrix.rows or matrix.columns are not defined', () => {
 	expect(() => checkMatrix([0])).toThrow(ReferenceError)
 	expect(() => checkMatrix({
-		0: 0,
-		columns: 1,
-		length: 1
-	})).toThrow(ReferenceError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: undefined,
-		columns: 1,
-		length: 1
-	})).toThrow(ReferenceError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		length: 1
-	})).toThrow(ReferenceError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: undefined,
-		length: 1
-	})).toThrow(ReferenceError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
+		data: [0],
 		columns: 1
 	})).toThrow(ReferenceError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
+		rows: undefined,
+		columns: 1
+	})).toThrow(ReferenceError)
+	expect(() => checkMatrix({
+		data: [0],
+		rows: 1
+	})).toThrow(ReferenceError)
+	expect(() => checkMatrix({
+		data: [0],
 		rows: 1,
-		columns: 1,
-		length: undefined
+		columns: undefined
 	})).toThrow(ReferenceError)
 })
 
-test('throw a TypeError if property rows, columns or length of argument matrix are not numbers', () => {
+test('Throw TypeError if matrix.rows or matrix.columns are not numbers', () => {
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: null,
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: true,
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: '1',
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: Symbol(),
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: {},
-		columns: 1,
-		length: 1
+		columns: 1
 	})).toThrow(TypeError)
 	
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: null,
-		length: 1
+		columns: null
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: true,
-		length: 1
+		columns: true
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: '1',
-		length: 1
+		columns: '1'
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: Symbol(),
-		length: 1
+		columns: Symbol()
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1,
-		columns: {},
-		length: 1
-	})).toThrow(TypeError)
-	
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: null
-	})).toThrow(TypeError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: true
-	})).toThrow(TypeError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: '1'
-	})).toThrow(TypeError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: Symbol()
-	})).toThrow(TypeError)
-	expect(() => checkMatrix({
-		0: 0,
-		rows: 1,
-		columns: 1,
-		length: {}
+		columns: {}
 	})).toThrow(TypeError)
 })
 
-test('throw a RangeError if property rows or columns of argument matrix is not an integer', () => {
+test('Throw RangeError if matrix.rows or matrix.columns are not integers', () => {
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
 		rows: 1.1,
-		columns: 1,
-		length: {}
+		columns: 1
 	})).toThrow(RangeError)
 	expect(() => checkMatrix({
-		0: 0,
+		data: [0],
+		rows: 1.9,
+		columns: 1
+	})).toThrow(RangeError)
+	
+	expect(() => checkMatrix({
+		data: [0],
 		rows: 1,
-		columns: 1.1,
-		length: {}
+		columns: 1.1
 	})).toThrow(RangeError)
-})
-
-test('throw a RangeError if property length of argument matrix does not equal property rows times property columns', () => {
 	expect(() => checkMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		rows: 2,
-		columns: 2,
-		length: 3
+		data: [0],
+		rows: 1,
+		columns: 1.9
 	})).toThrow(RangeError)
 })
 
-test('throw a TypeError if any enumerable property of argument matrix is not a number', () => {
+test('Throw ReferenceError if matrix.data.length is not defined', () => {
 	expect(() => checkMatrix({
-		0: undefined,
+		data: {
+			0: 0
+		},
 		rows: 1,
-		columns: 1,
-		length: 1
-	})).toThrow(TypeError)
+		columns: 1
+	})).toThrow(ReferenceError)
+	
 	expect(() => checkMatrix({
-		0: 0,
-		0: null,
-		rows: 2,
-		columns: 1,
-		length: 2
-	})).toThrow(TypeError)
-	expect(() => checkMatrix({
-		0: 0,
-		0: 0,
-		0: true,
+		data: {
+			0: 0,
+			length: undefined
+		},
 		rows: 1,
-		columns: 3,
-		length: 3
-	})).toThrow(TypeError)
+		columns: 1
+	})).toThrow(ReferenceError)
+})
+
+test('Throw RangeError if matrix.data.length does not equal matrix.rows * matrix.columns', () => {
 	expect(() => checkMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: '0',
+		data: {
+			0: 0,
+			1: 0,
+			2: 0,
+			3: 0,
+			length: 3
+		},
 		rows: 2,
-		columns: 2,
-		length: 4
+		columns: 2
+	})).toThrow(RangeError)
+	
+	expect(() => checkMatrix({
+		data: {
+			0: 0,
+			1: 0,
+			2: 0,
+			3: 0,
+			length: 5
+		},
+		rows: 2,
+		columns: 2
+	})).toThrow(RangeError)
+})
+
+test('Throw TypeError if any number indexed property of matrix.data is not a number', () => {
+	expect(() => checkMatrix({
+		data: [undefined],
+		rows: 1,
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: Symbol(),
+		data: [0, null],
+		rows: 2,
+		columns: 1
+	})).toThrow(TypeError)
+	expect(() => checkMatrix({
+		data: [0, 0, true],
+		rows: 1,
+		columns: 3
+	})).toThrow(TypeError)
+	expect(() => checkMatrix({
+		data: [0, 0, 0, '0'],
+		rows: 2,
+		columns: 2
+	})).toThrow(TypeError)
+	expect(() => checkMatrix({
+		data: [0, 0, 0, 0, Symbol()],
 		rows: 5,
-		columns: 1,
-		length: 5
+		columns: 1
 	})).toThrow(TypeError)
 	expect(() => checkMatrix({
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: 0,
-		0: {},
+		data: [0, 0, 0, 0, 0, {}],
 		rows: 2,
-		columns: 3,
-		length: 6
+		columns: 3
 	})).toThrow(TypeError)
+	expect(() => checkMatrix({
+		data: new Array(6),
+		rows: 2,
+		columns: 3
+	})).toThrow(TypeError)
+})
+
+test('return matrix if it fits the Matrix interface', () => {
+	const matrix = {
+		data: [0],
+		rows: 1,
+		columns: 1
+	}
+	
+	expect(checkMatrix(matrix)).toBe(matrix)
 })
