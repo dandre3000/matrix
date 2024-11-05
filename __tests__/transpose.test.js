a566a53a82b5a0642424d5b25de13e9ffa2bfdce
dandre3000 <deaundre.payne@gmail.com>
2024-11-05 02:01:36 -0500

    Update transpose.test.js

--- __tests__/transpose.test.js
+++ __tests__/transpose.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { Matrix } from '../src/Matrix.js'
 
-test('Throw Error if result is defined but not a Matrix instance, result.columns !== this.columns, or if result.columns !== this.columns', () => {
+test('Throw Error if result is defined but not a Matrix instance, result.rows !== this.rows, or if result.columns !== this.columns', () => {
 	const a = new Matrix(1, 2)
 	const b = new Matrix(2, 1)
 	
